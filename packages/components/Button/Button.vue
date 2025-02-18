e5338e64b5778aa9a95f023adf4e1b80ed2c9c64
李天泰 <1916689406@qq.com>
2025-02-18 18:41:47 +0800

    实现按钮

---
+++ packages/components/Button/Button.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+/**
+ * Button.vue
+ * Button.test.tsx
+ * types.ts
+ * style.css
+ * constants.ts
+ */
+
+defineOptions({
+  name: "ZsButton",
+});
+</script>
+
+<template>
+  <button style="background-color: blue; color: red">我的button</button>
+</template>
---
+++ packages/components/Button/index.ts
@@ -0,0 +1,4 @@
+import Button from "./Button.vue";
+import { withInstall } from "@zs-element/utils";
+
+export const ZsButton = withInstall(Button);
